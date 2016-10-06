d9041a8eadf8dac331cc857def6e9678b579cce1
fynn <iamfatefan000@gmail.com>
2016-10-07 04:05:34 +0800

    获取首页数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,9 @@
+const AV = require('../../utils/av-weapp-2.0.0-beta.0.js')
+AV.init({
+appId: "po7YWHB3bjxCwOkLEyBbO0Y6-gzGzoHsz",
+appKey: "NkA8kxpPmtK3NHwgy6vVBls0",
+});
+var allData = [];
 var index = {
     data:{
         imgUrls: [
@@ -10,33 +16,56 @@ var index = {
     interval: 5000,
     duration: 1000
     },
-    changeIndicatorDots: function(e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function(e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function(e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange: function(e) {
-    this.setData({
-      duration: e.detail.value
-    })
-  },
-  checkDetail: function (e) {
-      wx.navigateTo({url:'../../pages/detail/detail',success:function() {
-          console.info("跳转!")
-      }});
-  },
-  addCar: function (e) {
-    wx.navigateTo({url:'../../pages/myCar/myCar'})
-  }
+    onReady:function () {
+        var query = new AV.Query('FSCategory');
+        query.equalTo('f_app_id', 1);
+        query.find().then(queryCategoryCallback, function (error) {
+        });
+    },
+    checkDetail: function (e) {
+        wx.navigateTo({url:'../../pages/detail/detail',success:function() {
+            console.info("跳转!")
+        }});
+    },
+    addCar: function (e) {
+        wx.navigateTo({url:'../../pages/myCar/myCar'})
+    }
 };
+
+/**
+ * 分类查询 回调方法
+ */
+function queryCategoryCallback (results) {
+    if(results.length != 0) {
+        var queryProduce = new AV.Query('FSProduct');
+        for(let i = 0, l = results.length;i<l;i++) {
+            let p = {
+                f_category_name : results[i].attributes.f_category_name,
+                f_category_id : results[i].attributes.f_category_id,
+                data : []
+            };
+            allData.push(p);
+            console.info(p.f_category_id);                    
+            queryProduce.equalTo('f_category_id',p.f_category_id);
+            queryProduce.find().then(queryProduceCallback);
+        }
+    }
+};
+/**
+ * 产品查询 回调方法
+ */
+function queryProduceCallback (results) {
+    console.info('产品查询 回调方法');
+    if(results.length != 0) {
+        var category_id = results[0].attributes.f_category_id;
+        var i = 0,l = allData.length;
+        while (allData[i].f_category_id != category_id) {
+            ++i;
+        };
+        for(let a = 0,b = results.length; a<b;b++) {
+            allData[i].data.push(results[a].attributes);
+        }
+    };
+    console.info(allData);
+}
 Page(index);
